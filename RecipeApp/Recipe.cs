710b9fabae79b991dcd613e70d0a4377962fbbe1
David Filipiak <df239@canterbury.ac.uk>
2020-10-15 16:19:46 +0100

    Added attribute and property Description

--- RecipeApp/Recipe.cs
+++ RecipeApp/Recipe.cs
@@ -7,12 +7,19 @@ namespace RecipeApp
     class Recipe
     {
         private string name;
+        private string descripiton;
 
 
         public string Name
         {
             get { return this.name; }
             set { this.name = value; }
+        }
+
+        public string Description
+        {
+            get { return this.descripiton; }
+            set { this.descripiton = value; }
         }
     }
 }
